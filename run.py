311ee409e6906c20eaf404e84b8eeba3c25cedf6
altairmonroyjimenez-blip <altairmonroyjimenez@gmail.com>
2026-07-06 00:49:09 -0600

    Interfaz principal index - Día 6

--- run.py
+++ run.py
@@ -1,10 +1,6 @@
 from flask import Flask, render_template
-import os
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-TEMPLATE_DIR = os.path.join(BASE_DIR, "app", "templates")
-
-app = Flask(__name__, template_folder=TEMPLATE_DIR)
+app = Flask(__name__)
 
 @app.route("/")
 def inicio():
