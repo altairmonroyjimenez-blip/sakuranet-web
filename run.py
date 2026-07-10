a93346d58867df1c07684c6b6ce5c782b3afa196
altairmonroyjimenez-blip <altairmonroyjimenez@gmail.com>
2026-07-10 02:34:09 -0600

    agrgar pestañas y linkcs

--- run.py
+++ run.py
@@ -6,5 +6,30 @@ app = Flask(__name__)
 def inicio():
     return render_template("index.html")
 
+@app.route('/productos')
+def productos():
+    return render_template('productos.html')
+
+@app.route('/internet')
+def internet():
+    return render_template('internet.html')
+
+@app.route('/streaming')
+def streaming():
+    return render_template('streaming.html')
+
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
+
+# 🔥 Rutas que estaban abajo (las subimos)
+@app.route('/papeleria')
+def papeleria():
+    return render_template('papeleria.html')
+
+@app.route('/electronica')
+def electronica():
+    return render_template('electronica.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
